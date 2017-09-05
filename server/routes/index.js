42ff30ac89131d5cd8ac895813ba05373d27d0a3
Elizabeth Chan <elizabeth.schan@outlook.com>
2017-09-05 15:50:50 +0100

    attempted link to database

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,27 +3,33 @@ const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const usersController = require('../controllers').users;
 
 // app.engine('html', require('ejs').renderFile);
-app.set('views',  __dirname + '/server/views');
+app.set('views',  __dirname + '/../views');
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res, next) => {
   res.render('home');
+
   // res.send('\n\nHellllllo!\n\n');
 });
 
-app.get('/login', (req, res, next) => {
-  res.render('login');
+app.get('/users/new', (req, res, next) => {
+  res.render('users/signup');
+  // res.send('\n\nHellllllo!\n\n');
 });
 
-app.get('/signup', (req, res, next) => {
-  res.render('signup');
+app.get('/sessions/new', (req, res, next) => {
+  res.render('sessions/login');
+  // res.send('\n\nHellllllo!\n\n');
 });
 
 app.get('/homepage', (req, res) => {
   res.send('\n\nHellllllo!\n\n');
 });
+
+app.post('/users/new', usersController.create);
 
 app.listen(port, () => {
   console.log(`listening on port ${ port }`);
