5e321b3c81d7249fe4a8aaa7e69fbde0f20bb5ea
Olivia Frost <olivia.frost617@googlemail.com>
2017-09-07 10:47:01 +0100

    tests passing for homepage

--- test/test.js
+++ test/test.js
@@ -1,61 +1,62 @@
-const Browser = require('zombie');
+var expect = require('chai').expect,
+    assert = require('assert'),
+    Browser = require('zombie'),
+    browser = new Browser(),
+    url = 'http://localhost:3000/';
 
-// We're going to make requests to http://example.com/signup
-// Which will be routed to our test server localhost:3000
+
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../server/routes/index');
+var should = chai.should();
+
+chai.use(chaiHttp);
+
 Browser.localhost('example.com', 3000);
+
+
 
 describe('User visits signup page', function() {
 
   const browser = new Browser();
 
   before(function(done) {
-    browser.visit('/homepage', done);
+    browser.visit('/', done);
   });
 
-  describe('submits form', function() {
-
-    // before(function(done) {
-    //   browser
-    //     .fill('email',    'zombie@underworld.dead')
-    //     .fill('password', 'eat-the-living')
-    //     .pressButton('Sign Me Up!', done);
-    // });
-    //
-    // it('should be successful', function() {
-    //   browser.assert.success();
-    // });
+  describe('homepage loads', function() {
 
     it('should be successful', function() {
       browser.assert.success();
     });
 
-    it('should see the homeage', function() {
-      browser.assert.text('title', 'Index');
+    it('should see the homepage', function() {
+      expect(browser.text('body')).to.include('Makersbnb');
     });
   });
 });
 
 
-var assert = require('assert');
-// describe('Array', function() {
-//   describe('#indexOf()', function() {
-//     it('should return -1 when the value is not present', function() {
-//       assert.equal(-1, [1,2,3].indexOf(4));
-//     });
-//   });
-// });
 
-var chai = require('chai');
-var chaiHttp = require('chai-http');
-var server = require('../server/app');
-var should = chai.should();
+describe('User can sign up', function() {
 
-chai.use(chaiHttp);
+  const browser = new Browser();
 
-describe('Blobs', function() {
-  it('should list ALL blobs on /blobs GET');
-  it('should list a SINGLE blob on /blob/<id> GET');
-  it('should add a SINGLE blob on /blobs POST');
-  it('should update a SINGLE blob on /blob/<id> PUT');
-  it('should delete a SINGLE blob on /blob/<id> DELETE');
+  before(function(done) {
+    browser.visit('/users/new', done);
+  });
+
+  describe('submits form', function() {
+
+    it('fills in fields', function() {
+      browser.fill('username', 'pm')
+      browser.fill('name', 'Peter Malark')
+      browser.fill('email', 'hi@hi.com')
+      browser.fill('pw', 'iheartke')
+      browser.pressButton('Sign up')
+      expect(browser.text('body')).to.include('Peter Malark');
+    });
+  });
 });
+
+
